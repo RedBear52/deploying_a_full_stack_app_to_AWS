c75df04fcf0b90cc0428c06947650a9e57bb46f6
Ryan Spearman <94654287+RedBear52@users.noreply.github.com>
2023-05-17 11:42:36 -0500

    Fixed linting errors

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://fix-a-gram-api-dev.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://fix-a-gram-api-dev.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
