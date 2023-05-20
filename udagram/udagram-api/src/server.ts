24c5610b36c8a83f70967fe8dfe4c09cabe98821
Ryan Spearman <94654287+RedBear52@users.noreply.github.com>
2023-05-20 10:49:07 -0500

    Changed preflightContinue to 'false'

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -47,7 +47,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
       'Access-Control-Allow-Methods'
     ],
     "methods": "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-    "preflightContinue": true,
+    "preflightContinue": false,
     "origin": "*",
   }));
 
